4ce3c781f8aafcfc7354f75a3df9194ace5e19ee
Lawrence H Lee <Lee.LawrenceH@gmail.com>
2016-07-09 21:04:16 -0700

    Add skeleton for some tests

---
+++ finwic/filename_parser.py
@@ -0,0 +1,11 @@
+def strip_extension:
+    pass
+
+def parse_upper_camel_case:
+    pass
+
+def parse_lower_camel_cass:
+    pass
+
+def parse_snake_case:
+    pass
---
+++ finwic/tests/filename_parser_test.py
@@ -0,0 +1,11 @@
+def strip_extension_test:
+    pass
+
+def parse_upper_camel_case_test:
+    pass
+
+def parse_lower_camel_cass_test:
+    pass
+
+def parse_snake_case_test:
+    pass
