ba009922843dd6be09de5dd2ce90a7266eb38a4f
Lawrence H Lee <Lee.LawrenceH@gmail.com>
2016-07-09 21:44:40 -0700

    Add some tests

--- finwic/filename_parser.py
+++ finwic/filename_parser.py
@@ -1,11 +1,17 @@
-def strip_extension:
+def strip_extension(filename):
+    if filename[0] == '.':
+        return filename[1:]
+    else:
+        return filename.split('.')[0]
+
+def parse_upper_camel_case(word):
     pass
 
-def parse_upper_camel_case:
+def parse_lower_camel_case(word):
     pass
 
-def parse_lower_camel_cass:
-    pass
+def parse_snake_case(word):
+    return word.split('_')
 
-def parse_snake_case:
-    pass
+def parse_kebab_case(word):
+    return word.split('-')
--- finwic/tests/filename_parser_test.py
+++ finwic/tests/filename_parser_test.py
@@ -1,11 +1,37 @@
-def strip_extension_test:
-    pass
+from finwic.filename_parser import *
 
-def parse_upper_camel_case_test:
-    pass
+def strip_extension_test():
+    filename = "bob.html.erb"
+    assert strip_extension(filename) == "bob"
+    filename = ".config"
+    assert strip_extension(filename) == "config"
 
-def parse_lower_camel_cass_test:
-    pass
+def parse_upper_camel_case_test():
+    word = "GoneWithTheWind"
+    assert parse_upper_camel_case(word) == ["gone", "with", "the", "wind"]
+    word = "ABigMistake"
+    assert parse_upper_camel_case(word) == ["a","big", "mistake"]
+    word = "IncorrectHTTPString"
+    assert parse_upper_camel_case(word) == ["incorrect", "http", "string"]
 
-def parse_snake_case_test:
-    pass
+def parse_lower_camel_cass_test():
+    word = "goneWithTheWind"
+    assert parse_lower_camel_case(word) == ["gone", "with", "the", "wind"]
+    word = "httpYonderBo"
+    assert parse_lower_camel_case(word) == ["http", "yonder", "bo"]
+    word = "incorrectHTTPString"
+    assert parse_lower_camel_case(word) == ["incorrect", "http", "string"]
+    word = "bob"
+    assert parse_lower_camel_case(word) == ["bob"]
+
+def parse_snake_case_test():
+    word = "anyways"
+    assert parse_snake_case(word) == ["anyways"]
+    word = "little_Bobby_TABLES"
+    assert parse_snake_case(word) == ["little", "bobby", "tables"]
+
+def parse_kebab_case_test():
+    word = "anyways"
+    assert parse_snake_case(word) == ["anyways"]
+    word = "little-Bobby_TABLES-no"
+    assert parse_snake_case(word) == ["little", "bobby_tables", "no"]
